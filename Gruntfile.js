488a8d71b05192520aa07c8036490e0a77321cf2
Jeremy Shute <j@altschool.com>
2013-12-16 14:08:06 -0500

    amber example

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+	var library_home = "bower_components/"
+	  , amber_home = library_home + "amber";
+
+	grunt.loadNpmTasks("grunt-contrib-requirejs");
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		requirejs: {
+			compile: {
+				options: {
+					paths: {
+            "amber_vm": amber_home + "/support",
+            'amber_lib': library_home,
+            "amber_core": amber_home + "/js",
+            "amber_core/_source": amber_home + "/st",
+            "jquery": library_home + "jquery/jquery.min"
+					},
+					name: "deploy",
+					optimize: "uglify2",
+					out: "deploy.min.js"
+				}
+			}
+		}
+	});
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+amber-example
+=============
+
+Installation:
+
+    $ npm install
+    $ bower install
+
+Want to hack on your project?  Use Helios!
+
+    $ ./bower_components/amber/bin/amber serve
+    $ open http://127.0.0.1:4000/devel.html
+
+Want to run your project?
+
+    $ ./bower_components/amber/bin/amber serve
+    $ open http://127.0.0.1:4000
+
+Want to make deploy.min.js?
+
+    $ grunt requirejs
